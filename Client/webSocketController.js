c7d48399fcb100ae6b6c8f7a3e3014cf8a090916
Angel <angeldm03@hotmail.com>
2018-05-05 04:14:03 -0700

    Finished functionality of game

---
+++ Client/webSocketController.js
@@ -0,0 +1,132 @@
+var score = 0;
+// DEFINE UI ELEMENTS
+var clientOutgoing;
+var serverResponse;
+var clientServerResult;
+
+var serverIncoming;
+var clientResponse;
+var serverClientResult;
+
+var scoreUI;
+
+var ws;
+
+function setupConnection(){
+    clientOutgoing = document.getElementById("client-outgoing");
+    serverResponse = document.getElementById("server-response");
+    clientServerResult = document.getElementById("client-server-result");
+
+    serverIncoming = document.getElementById("server-incoming");
+    clientResponse = document.getElementById("client-response");
+    serverClientResult = document.getElementById("server-client-result");
+
+    scoreUI = document.getElementById("score");
+    ws = new WebSocket("ws://127.0.0.1:9999/");
+    
+    ws.addEventListener("open",()=>{
+        console.log("Connection started");
+    },false);
+
+    ws.addEventListener("message", (e) => {
+        
+        var str = e.data;
+        var msg = parseMsg(str);
+        switch(msg.type){
+            case "client": {
+                console.log("Received a response to client message: " + msg.responseValue);
+                serverResponse.innerHTML = "Server: "+msg.responseValue;
+                var gameResult = checkWin(msg.requestValue, msg.responseValue);
+                if(gameResult > 0){
+                        console.log("You won");
+                        clientServerResult.innerHTML = "WON!! =D";
+                }
+                else if(gameResult === 0){
+                    console.log("You tied");
+                    clientServerResult.innerHTML = "DRAW!! :|";
+                }
+                else {
+                    console.log("You lost");
+                    clientServerResult.innerHTML = "LOST!! :(";
+                }
+                break;
+            }
+            case "server":{
+                console.log("Received a new server message: " + msg.responseValue);
+                serverIncoming.innerHTML = "Server: "+msg.responseValue;
+                var clientChoice = pickRandomHand();
+                clientResponse.innerHTML = "Client: "+clientChoice;
+                var gameResult = checkWin(clientChoice, msg.responseValue);
+                if (gameResult > 0) {
+                    console.log("You won");
+                    serverClientResult.innerHTML = "WON!! =D";
+                }
+                else if (gameResult === 0) {
+                    console.log("You tied");
+                    serverClientResult.innerHTML = "DRAW!! :|";
+                }
+                else {
+                    console.log("You lost");
+                    serverClientResult.innerHTML = "LOST!! :(";
+                }
+                break;
+            }
+        }
+        
+        score += gameResult;
+        scoreUI.innerHTML = score;
+    },false);
+
+    ws.addEventListener("close", () => {
+        console.log("Connection closed");
+    },false);
+
+    ws.addEventListener("error", (e) => {
+        console.log("The connection failed");
+
+    }, false);
+}
+
+// INITIALIZE GAME:
+window.addEventListener("load",setupConnection,false);
+
+// ON HTML FUNCTIONS:
+function sendChoice(selection){
+    
+    clientOutgoing.innerHTML = "You: "+selection;
+    ws.send(selection);
+}
+
+function parseMsg(str){
+    var splitMsg = str.split('-');
+    var msg = {};
+    msg.type = splitMsg[0];
+    msg.requestValue = splitMsg[1];
+    msg.responseValue = splitMsg[2];
+
+    return msg;
+}
+
+// Checks the outcome of a rock paper scissor game
+function checkWin(playerOne, playerTwo){
+    console.log("comparing: "+playerOne+" "+playerTwo);
+   if(playerOne == playerTwo) return 0;
+   
+   if((playerOne === "Rock" && playerTwo === "Scissors") 
+   || (playerOne === "Paper" && playerTwo === "Rock")
+   || (playerOne === "Scissors" && playerTwo === "Paper"))
+   return 1;
+   
+   return -1
+}
+
+function pickRandomHand() {
+    var randomChoice = Math.floor((Math.random() * 3) + 1);
+    var choice = "";
+    switch (randomChoice) {
+        case 1: { choice = "Rock"; break; }
+        case 2: { choice = "Paper"; break; }
+        case 3: { choice = "Scissors"; break; }
+    }
+    return choice;
+}
--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,76 @@
 var WebSocketServer = require("websocket").server;
-
 var server = require("http").createServer();
+var clients = [];
 
+// Configure the websocket
 var ws = new WebSocketServer({
     httpServer : server,
     autoAcceptConnections:true
 });
 
-console.log("Chat Server listening on port 9999:");
-
+// Start the WebSocket and server
+ws.on("connect",connectHandler);
 server.listen(9999);
+console.log("RockPaperScissor listening on port 9999:");
+
+// Set a timeout for server to play with a random client every 3 seconds
+setTimeout(sendMessage, 3000);
+
+// HANDLER FUNCTIONS //
+function connectHandler(conn){
+    // identify the connected client
+    conn.identifier = conn.remoteAddress;
+    conn.on("message",messageHandler);
+    conn.on("close",closeHandler);
+    clients.push(conn);
+    console.log("New Client connected: "+conn.identifier);
+}
+
+function messageHandler(message){
+    var clientChoice = message.utf8Data.toString();
+    console.log("Received message from client: "+this.identifier+" message: "+clientChoice);
+    var serverChoice = pickRandomHand();
+    this.sendUTF("client-"+clientChoice+"-"+serverChoice);
+    console.log("Answered with: "+serverChoice);
+}
+
+function closeHandler(){
+    // Find the closed client connection in our list of clients
+    var index = clients.indexOf(this);
+    var clientID = clients[index].identifier;
+    console.log("Client " + clientID + " has closed connection");
+    if (index < 0){
+        console.log("The client " + clientID + " was not found in the server list of clients");
+        return;
+    }
+
+    clients.splice(index,1);
+    console.log("Client "+clientID+" was removed successfully from registered clients");
+}
+
+function sendMessage(){
+    // Setting another timer for next play
+    var timer = ((Math.random() * 5)+2) * 1000;
+    setTimeout(sendMessage, timer);
+    // Make sure there are connected users to play with
+    if(clients.length <= 0) return;
+
+    // Randomly pick a user to play with
+    var clientIndex = Math.floor(Math.random()*clients.length);
+    var client = clients[clientIndex];
+    var msg = pickRandomHand();
+    client.sendUTF("server--"+msg);
+    console.log("Picked: "+msg+" Sent to: "+client.identifier);
+    
+}
+
+function pickRandomHand(){
+    var randomChoice = Math.floor((Math.random() * 3) + 1);
+    var serverChoice = "";
+    switch (randomChoice) {
+        case 1: { serverChoice = "Rock"; break; }
+        case 2: { serverChoice = "Paper"; break; }
+        case 3: { serverChoice = "Scissors"; break; }
+    }
+    return serverChoice;
+}
